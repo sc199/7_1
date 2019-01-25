873e9a7f80cb9fe369360a06a641ddec5cef7a18
sc199 <47023620+sc199@users.noreply.github.com>
2019-01-25 12:39:38 +0400

    პაატა მამფორია

---
+++ swap.py
@@ -0,0 +1,16 @@
+a = 2
+b = 5
+
+print("a =", a)
+print("b =", b)
+
+##temp = a    # a-s da b-s vucvli mnishvnelobebs
+##a = b
+##b = temp
+
+##print("a =", a)
+##print("b =", b)
+
+a, b = b, a   # mnishvnelobebis gacvla
+print("a =", a)
+print("b =", b)
